6edd5dda2e0a384f7e7a14fdecfa48a311dc73fd
AhmetGokcimen <agokcimen2811@gmail.com>
2026-08-13 11:11:31 +0300

    Add working API-based data collection for both categories

--- basics.py
+++ basics.py
@@ -2,17 +2,21 @@ import requests
 import truststore
 truststore.inject_into_ssl()
 import os
+import json
 
+API_URL = "https://www.ilan.gov.tr/api/api/services/app/Ad/AdsByFilter"
 category_list = [
-    {"name": "İflas Hukuku Davaları", "url": "https://www.ilan.gov.tr/ilan/kategori/50/iflas-ve-tasfiye-ilanlari", "slug": "bankruptcy"},
-    {"name": "Konkordato ve Mühlet", "url": "https://www.ilan.gov.tr/ilan/kategori/51/konkordato-ve-muhlet", "slug": "concordat"}
+    {"name": "İflas Hukuku Davaları", "url": "https://www.ilan.gov.tr/ilan/kategori/50/iflas-ve-tasfiye-ilanlari", "slug": "bankruptcy", "tax_id": 50},
+    {"name": "Konkordato ve Mühlet", "url": "https://www.ilan.gov.tr/ilan/kategori/49/konkordato-ve-muhlet-iik-288inci-md", "slug": "concordat", "tax_id": 49}
 ]
 
 print(os.getcwd()) 
 for category in category_list:
-    response = requests.get(category['url'])
+    payload ={"keys": {"txv": [category['tax_id']]}, "skipCount": 0, "maxResultCount": 20}
+    response = requests.post(API_URL, json=payload)
     print(f"Category Name: {category['name']}, URL: {category['url']}, Status Code: {response.status_code}")
+    data = response.json()
 
-    with open(f"raw_html/{category['slug']}.html", "w", encoding="utf-8") as f:
-        f.write(response.text)
-        print(os.path.abspath(f"raw_html/{category['slug']}.html"))
+    with open(f"raw_html/{category['slug']}.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+        print(os.path.abspath(f"raw_html/{category['slug']}.json"))
